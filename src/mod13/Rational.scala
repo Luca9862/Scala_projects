cf9250cd7c2069522da1c8a304688a01539544a7
canali.luca <canali.luca998@gmail.com>
2022-11-01 09:52:45 +0100

    Update Rational.scala

--- src/mod13/Rational.scala
+++ src/mod13/Rational.scala
@@ -1,12 +1,26 @@
 package mod13
 
-class Rational (x: Int, y:Int){
+//definizione della classe Rational illustrata nel modulo 13
 
-  override def toString(): String={
-    if(y==1) x.toString else x + "/" + y
-  }
+class Rational(x: Int, y: Int) {
+  require(y>0, "denominator must be positive")
+  val num = x / abs(gcd(x, y))
+  val den = y / abs(gcd(x, y))
 
-  def add(that: Rational):Rational={
-    new Rational(x * that.y + that.x * y, y * that.y)
-  }
+  def this(x: Int) = this(x, 1)
+
+  def abs(a: Int): Int =
+    if (a < 0) -a else a
+
+  def gcd(a: Int, b: Int): Int =
+    if (b == 0) a else gcd(b, a % b)
+
+
+  override def toString(): String =
+    if (this.y == 1) this.num.toString else this.num + "/" + this.den
+
+
+  def add(that: Rational): Rational =
+    new Rational(this.num * that.den + that.num * this.den, this.den * that.den)
+
 }
